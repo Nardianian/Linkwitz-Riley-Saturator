ad9c385a51edb436f676f89a166f7c7243753135
Nardianian <145938851+Nardianian@users.noreply.github.com>
2026-02-21 19:45:22 +0100

    Remove commented-out code and clean up formatting

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -25,7 +25,6 @@ public:
     void resized() override;
 
 private:
-    // This reference is provided as a quick way for your editor to access the processor object that created it.
     LR_SaturatorAudioProcessor& audioProcessor;
 
     juce::Slider crossoverSlider, gainSlider, mixSlider;
@@ -34,4 +33,3 @@ private:
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (LR_SaturatorAudioProcessorEditor)
 };
-
